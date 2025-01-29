d84f84667b4e3e3ac8af9722d1bfb641be09745f
GowJack0309102 <0309102g@acadiau.ca>
2025-01-29 17:28:26 -0400

    commit #1
    new python file, prompts user to add first two marks, calculates total and outputs the value of total

---
+++ percentAverage.py
@@ -0,0 +1,6 @@
+m1 = int(input("Please enter mark #1: "))
+m2 = int(input("Please enter mark #2: "))
+# TODO, add user prompt for mark 3
+
+total = m1 + m2 # plus 3rd mark
+print("Total: ",str(total)+"\n")
